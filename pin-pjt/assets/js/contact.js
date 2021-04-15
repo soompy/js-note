988367c88d482b55906f4b51abaf9b59e2b140c6
smartscore_smlee <smlee@smartscore.kr>
2021-04-15 13:04:10 +0900

    pp 작업

--- pin-pjt/assets/js/contact.js
+++ pin-pjt/assets/js/contact.js
@@ -1,13 +1,17 @@
-// hover 하면 해당 drop-down에 show클래스가 추가
+function songpa() {
+    var popup1 = document.getElementById("songpaPopup");
+    popup1.classList.add("show");
+}
 
-//  여기에 마우스를 올리면
-var dropDown = document.querySelector('.drop-down-box');
-// 여기에 show 클래스가 추가된다
-var dropDownContent = document.querySelector('.drop-down');
-var contentParent = dropDownContent.parentElement;
-var currentBox = 0;
 
-contentParent.addEventListener('mouseover', function () {
-    this.dropDownContent.classList.add('show');    
-})
+function geumgok() {
+    var popup2 = document.getElementById("geumgokPopup");
+    popup2.classList.add("show");
+}
 
+function closed() {
+    var popup1 = document.getElementById("songpaPopup");
+    var popup2 = document.getElementById("geumgokPopup");
+    popup1.classList.remove("show");
+    popup2.classList.remove("show");
+}
