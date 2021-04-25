837b5d6aed03493ebc7de8e8e03414dab285c2c4
soompy <sooper1137@gmail.com>
2021-04-25 19:49:01 +0900

    visual banner, footer filter

--- wrich/assets/js/ui.js
+++ wrich/assets/js/ui.js
@@ -1,10 +1,14 @@
 // .form - field - raise를 누르면.field - select - cont가 나타나고 여기에 마우스를 떼면
 // .field - select - cont가 사라짐
 
-var familySite = document.querySelector('.form-field-raise');
-var selectFamily = document.querySelector('.field-select-cont');
+// var familySite = document.querySelector('.form-field-raise');
+// var selectFamily = document.querySelector('.field-select-cont');
 
-selectFamily.style.display = "none";
-familySite.addEventListener('click', function () {
-    selectFamily.style.display = "block";
-})
+
+// familySite.addEventListener('click', function () {
+//     this.classList = "form-field-raise select-box wd-200 active";
+//     selectFamily.addEventListener('mouseover', e => {
+//         familySite.classList = "form-field-raise select-box wd-200";
+//     });
+// })
+
